51465ebae49b998c441c3bf23d4f0293c520267d
hienhg <giahienhoang99@gmail.com>
2026-04-02 21:59:35 -0400

    no more hardcoded collectionPrefix (was hardcoded to access firebase prod data from local)

--- src/storage/engines/types.ts
+++ src/storage/engines/types.ts
@@ -107,8 +107,7 @@ export abstract class StorageEngine {
     name: 'revisit',
   });
 
-  // protected collectionPrefix = import.meta.env.DEV ? 'dev-' : 'prod-';
-  protected collectionPrefix = 'prod-';
+  protected collectionPrefix = import.meta.env.DEV ? 'dev-' : 'prod-';
 
   protected studyId: string | undefined;
 
